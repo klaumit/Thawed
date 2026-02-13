3810409f4411470adbb02e2d672e56e1a36b11e3
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-02-13 21:32:19 +0100

    Written all stubs

--- src/Extracting/Extractors/IcedExtractor.cs
+++ src/Extracting/Extractors/IcedExtractor.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Extracting.API;
+using Iced.Intel;
 
 namespace Extracting.Extractors
 {
@@ -8,11 +9,24 @@ namespace Extracting.Extractors
     {
         public Task<object> Decode(IEnumerable<byte[]> byteArrays)
         {
-            
-            
-            
-            
-            
+            var res = DecodeSync(byteArrays);
+            return Task.FromResult(res);
+        }
+
+        public object DecodeSync(IEnumerable<byte[]> byteArrays)
+        {
+            foreach (var bytes in byteArrays)
+            {
+                const int bit = 16;
+                var reader = new ByteArrayCodeReader(bytes);
+                const ulong ip = 0;
+                const DecoderOptions opt = DecoderOptions.NoInvalidCheck;
+                var decoder = Decoder.Create(bit, reader, ip, opt);
+                var instr = decoder.Decode();
+
+                throw new System.NotImplementedException(instr.ToString());
+            }
+
             throw new System.NotImplementedException();
         }
     }
