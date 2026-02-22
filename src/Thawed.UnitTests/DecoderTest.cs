6c5d37b5f62cff84cf9b668a25a3d940ea883743
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-02-22 01:20:31 +0100

    Filtering for extracted hex

--- src/Thawed.UnitTests/DecoderTest.cs
+++ src/Thawed.UnitTests/DecoderTest.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Generator.Meta;
+using Generator.Tools;
 using Thawed.Auto;
 using Xunit;
+using static Generator.Tools.CsvTool;
 
 namespace Thawed.UnitTests
 {
@@ -12,24 +16,24 @@ namespace Thawed.UnitTests
         public static IEnumerable<object[]> AllOpcodes =>
             Enum.GetValues<Opcode>().Except([default]).Take(3).Select(d => new object[] { d });
 
+        private const StringComparison Inv = StringComparison.InvariantCultureIgnoreCase;
+        private static readonly string Rot = FileTool.GetPath<DecoderTest>();
+        private static readonly string Res = Path.Combine(Rot, "Resources");
+
         [Theory]
         [MemberData(nameof(AllOpcodes))]
         public async Task ShouldDecode(Opcode op)
         {
-            var cpu = op.ToString().ToLowerInvariant();
-            
-         
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            throw new InvalidOperationException(cpu + " !");
+            var opT = op.ToString();
+            var csv = FromFile<Extracted>(Path.Combine(Res, "Win.csv"))
+                .Where(x => opT.Equals(x.Op, Inv)).ToArray();
+            foreach (var one in csv)
+            {
+                var input = Convert.FromHexString(one.Input!);
+                var output = $"{one.Op} {one.Arg}".Trim();
 
+                throw new InvalidOperationException($"{Convert.ToHexString(input)} => {output}");
+            }
         }
     }
 }
