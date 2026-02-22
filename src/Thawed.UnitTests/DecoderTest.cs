ada13aa93570766dd43fac65fb115dc91e1d1afb
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-02-22 02:02:06 +0100

    Adding code

--- src/Thawed.UnitTests/DecoderTest.cs
+++ src/Thawed.UnitTests/DecoderTest.cs
@@ -5,7 +5,16 @@ using System.Threading.Tasks;
 using Generator.Meta;
 using Thawed.Auto;
 using Xunit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generator;
+using Xunit;
 using T = Thawed.UnitTests.TestTool;
+using static Generator.Tools.FileTool;
 
 namespace Thawed.UnitTests
 {
@@ -19,7 +28,10 @@ namespace Thawed.UnitTests
         public async Task ShouldDecode(Opcode op)
         {
             var opT = op.ToString().ToUpperInvariant();
-            foreach (var one in ExW[opT])
+            var ones = ExW[opT];
+            Assert.True(ones.Length >= 1, $"{ones.Length} ?!");
+
+            foreach (var one in ones)
             {
                 var input = Convert.FromHexString(one.Input!);
                 var output = $"{one.Op} {one.Arg}".Trim();
