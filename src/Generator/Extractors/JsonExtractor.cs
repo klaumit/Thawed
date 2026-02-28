966e03c381e4da92ce52d0abba40578ca7dd70fd
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-02-28 13:16:36 +0100

    Allow chunking in json cache

--- src/Generator/Extractors/JsonExtractor.cs
+++ src/Generator/Extractors/JsonExtractor.cs
@@ -23,34 +23,46 @@ namespace Generator.Extractors
 
         public async IAsyncEnumerable<Decoded[]> Decode(IEnumerable<byte[]> byteArrays)
         {
-            foreach (var byteArray in byteArrays)
+            if (byteArrays is not IReadOnlyList<byte[]> list)
+                throw new InvalidOperationException($"Not a list! {byteArrays.GetType().FullName}");
+            
+            var ba = list
+                .Select(l => (a: l, c: IsArgCached(l, out var d) ? d : null))
+                .ToArray();
+            
+            var cached = ba.Where(y => y.c != null).Select(x => x.c);
+            foreach (var item in cached)
             {
-                var hex = byteArray.ToStr();
-                if (_cache.TryGetValue(hex, out var found))
-                {
-                    yield return [found];
-                    continue;
-                }
-                if (_cache.FirstOrDefault(e => hex.StartsWith(e.Key))
-                    is { Value: not null } partly)
-                {
-                    yield return [partly.Value];
-                    continue;
-                }
-                await foreach (var real in _extractor.Decode([byteArray]))
-                {
-                    var isDirty = false;
-                    foreach (var decoded in real)
-                    {
-                        if (decoded.Offset != 0) continue;
-                        var rHex = decoded.Hex;
-                        if (!_cache.ContainsKey(rHex)) isDirty = true;
-                        _cache[rHex] = decoded;
-                    }
-                    if (isDirty) Save();
-                    yield return real;
-                }
+                yield return [item!];
             }
+            
+            var missing = ba.Where(y => y.c == null).Select(x => x.a);
+            await foreach (var real in _extractor.Decode(missing))
+            {
+                var isDirty = false;
+                foreach (var decoded in real)
+                {
+                    if (decoded.Offset != 0) continue;
+                    var rHex = decoded.Hex;
+                    if (!_cache.ContainsKey(rHex)) isDirty = true;
+                    _cache[rHex] = decoded;
+                }
+                if (isDirty) Save();
+                yield return real;
+            }
+        }
+
+        private bool IsArgCached(byte[] array, out Decoded? found)
+        {
+            var hex = array.ToStr();
+            if (_cache.TryGetValue(hex, out found))
+                return true;
+
+            if (_cache.FirstOrDefault(e => hex.StartsWith(e.Key))
+                    is var (_, val) && (found = val) != null)
+                return true;
+
+            return false;
         }
 
         private void Save()
