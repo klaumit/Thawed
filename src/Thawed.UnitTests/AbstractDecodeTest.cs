de4337be4802ee2acb7b42ba534f1f487af8b992
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-03-11 23:21:40 +0100

    Finished test

--- src/Thawed.UnitTests/AbstractDecodeTest.cs
+++ src/Thawed.UnitTests/AbstractDecodeTest.cs
@@ -1,18 +1,23 @@
 using System;
 using Generator.Tools;
+using Xunit;
 
 namespace Thawed.UnitTests
 {
     public abstract class AbstractDecodeTest
     {
+        private static readonly IDecoder Decoder = Decoders.GetDecoder();
+
         protected static void AssertDecode(string bin, string op, string arg)
         {
             var bytes = BitTool.ParseBin(bin);
             var hex = Convert.ToHexString(bytes);
             var ins = IceTool.Parse16(bytes);
-            var txt = $"{op} {arg}".Trim();
-
-            throw new NotImplementedException(hex + " / " + bin + " / " + txt + " / " + ins);
+            var reader = new ArrayReader(bytes);
+            var decoded = Decoder.Decode(reader, fail: true);
+            var expected = $"{op} {arg}".Trim();
+            var actual = decoded?.ToString();
+            Assert.Equal(expected, actual);
         }
     }
 }
