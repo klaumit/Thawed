53eb61a6b9c9d506d34c45f667481b4ab35c1b50
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-03-14 01:33:04 +0100

    Adding two more

---
+++ src/Generator/Extractors/WinExtractor3.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CliWrap;
+using CliWrap.Buffered;
+using Generator.API;
+using Generator.Tools;
+
+namespace Generator.Extractors
+{
+    public sealed class WinNiExtractor : WinBaseExtractor, IExtractor
+    {
+        public override IAsyncEnumerable<Decoded[]> Decode(IEnumerable<byte[]> byteArrays)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
